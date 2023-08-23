bf4e465a40f581fdb3d322cabc939e55613631d5
Wriar <81789627+Wriar@users.noreply.github.com>
2023-08-22 20:57:14 -0600

    Update filemanager-listeners.js

--- src/public/js/filemanager-listeners.js
+++ src/public/js/filemanager-listeners.js
@@ -1,5 +1,6 @@
 document.getElementById('openNav').addEventListener('click', function (e) {
     if (e.target.classList.contains('openNav')) {
+        // eslint-disable-next-line no-undef
         openNav();
     }
 });
