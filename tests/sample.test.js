86cdd1270766cc31388789edd5b979aba3d26463
Wriar <81789627+Wriar@users.noreply.github.com>
2023-08-27 21:08:59 -0600

    include test suite folder

---
+++ tests/sample.test.js
@@ -0,0 +1,10 @@
+/* eslint-disable no-undef */
+
+describe('Sample Test Suite', () => {
+    it('should always return OK', () => {
+      // TODO, this just verifies that the test suite is working.
+      const result = 'OK';
+      expect(result).toBe('OK');
+    });
+  });
+  
