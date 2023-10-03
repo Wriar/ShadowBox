499f8601bcdc9bf899b3eccbc5953d2bc946ad77
Wriar <81789627+Wriar@users.noreply.github.com>
2023-10-02 21:44:28 -0600

    add scrollable filetable, file icons-silly cssjs

---
+++ src/public/ico/files/LICENSE.md
@@ -0,0 +1,6 @@
+Certain icons used in this software are licensed under the Icons8 usage license.
+
+For more information, please visit https://icons8.com/license
+
+Refer to ``license.md`` in the main directory for this project's license.
+
--- src/public/js/filemanager-styles.js
+++ src/public/js/filemanager-styles.js
@@ -93,3 +93,45 @@ const window_element_resize_observer = new ResizeObserver(function (entries) {
 });
 
 window_element_resize_observer.observe(body);
+
+/**
+ * As the file table height is difficult to set to completely fill the remainder of the page
+ * with pure css, this function will set the height of the file table to the height of the viewport.
+ *
+ * If the number of <tr> items * the height of each <tr> item is greater than the height of the viewport,
+ * we will use the dynamic resizing height of the file table. This prevents the file tables from overflowing and causing
+ * vertical viewport scroll.
+ *
+ * Otherwise, the file table is left to the default 100% height. This prevents each file item taking up
+ * ample vertical space and being distributed evenly (vertically).
+ */
+function setFileTableHeight() {
+    //Get the y position of the first item with class=pathContainer
+    const pathContainer = document.getElementsByClassName("pathContainer")[0];
+    const pathContainerY = pathContainer.getBoundingClientRect().y;
+    const pathContainerHeight = pathContainer.getBoundingClientRect().height;
+    const pathContainerTotalVerticalPadding = parseInt(window.getComputedStyle(pathContainer).getPropertyValue('padding-top')) + parseInt(window.getComputedStyle(pathContainer).getPropertyValue('padding-bottom'));
+    const fileTableTopBuffer = Math.floor((pathContainerY + pathContainerHeight + pathContainerTotalVerticalPadding) * 1.2);
+    const viewportHeight = window.innerHeight;
+    const fileTableHeight = viewportHeight - fileTableTopBuffer;
+
+    //First, check if the file table will overflow. Compute this by getting the number of <tr> items in the file table, and multiplying it by the height of each <tr> item.
+    //If the <tr> item height * number of <tr> items is greater than the height of the file table, then we need to set the height of the file table to the height of the viewport minus the height of the path container.
+    //Otherwise, leave height as relative 100%.
+    const fileTable = document.getElementById("file-browser");
+    const fileTableTrItems = fileTable.getElementsByTagName("tr");
+    const fileTableTrItemHeight = fileTableTrItems[0].getBoundingClientRect().height;
+    const totalFileTableHeight = fileTableTrItemHeight * fileTableTrItems.length;
+
+    //If the file table height is greater than the viewport height, set the file table height to the viewport height minus the path container height.
+    if(totalFileTableHeight <= fileTableHeight) {
+        document.getElementById("file-browser-container").style.height = "100%";
+        return;
+    }
+
+    document.getElementById("file-browser-container").style.height = fileTableHeight + "px";
+}
+
+setFileTableHeight();
+//Whenever window is resized, set the file table height.
+window.addEventListener('resize', setFileTableHeight);
